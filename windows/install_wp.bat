da9512036ab4c1ccc26c8c9c71902018b4a6fed8
runlevel4 <edwardsj9090@gmail.com>
2021-03-11 16:32:29 -0500

    Update install_wp.bat

--- windows/install_wp.bat
+++ windows/install_wp.bat
@@ -4,6 +4,10 @@ echo.
 echo Enabling IIS (Windows Web Server)...
 dism /online /Enable-Feature /FeatureName:IIS-WebServer /All
 echo.
+:: WORK IN PROGRESS
+:: 
+:: GOING TO ADD IN THE PIECE THAT CREATES THE VIRTUAL DIRECTORIES IN IIS LATER ON
+::
 echo This will download and install wordpress...
 echo.
 echo Downloading and extracting latest WordPress zip file to the user's desktop...
